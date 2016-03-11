8239a925b4c3efcdca5c38baed3b638a327d9f26
허정무 <magic_eye92@naver.com>
2016-03-11 15:42:29 +0900

    V-Version

--- ex01/ex01.py
+++ ex01/ex01.py
@@ -1,10 +1,10 @@
-# print "Hello World"
-# print "Hello Again"
-# print "I like typing this."
-# print "This is fun."
-# print 'Yay! printing.'
-# print "I'd much rather you 'not'."
-# print 'I "said" do not touch this.'
+print "Hello World"
+print "Hello Again"
+print "like typing this."
+print "This is fun."
+print 'Yay! printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
 
 print "JUNGMOO HUR"
 print "1590 OREGON ST 12A, berkeley, CA"
