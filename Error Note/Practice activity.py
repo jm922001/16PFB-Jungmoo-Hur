123efb6788c998674fb09ba3f9af762cb46f1568
CAD Client <CAD Client>
2016-04-08 16:18:30 +0900

    Pratice Acticity

---
+++ Error Note/Practice activity.py
@@ -0,0 +1,9 @@
+print "You mother fucker."
+print 3+2+2
+print 7-2
+
+print 1<2
+print 5<3
+
+print 3<=5
+
--- ex13/ex13.py
+++ ex13/ex13.py
@@ -1,9 +1,10 @@
 from sys import argv
 
 print("argv=" + str(argv))
-script, first, second, third = argv
+script, first, second, third, fourth = argv
 
 print ("The script is called: %s" % script)
 print ("Your first variable is: %s" %  first)
 print ("Your second variable is: %s" %  second)
 print ("Your third variable is: %s" %  third)
+print ("I'm gonna be the rich man: %s " %fourth)
